45242a99ace11661b45527a09eb2b889ac659706
yuandingsansheng <975375822@qq.com>
2018-10-12 15:31:38 +0800

    3

--- pages/dubSingle/dubSingle.js
+++ pages/dubSingle/dubSingle.js
@@ -14,8 +14,10 @@ Page({
         paragraph: 'I have a dream that one day this nation will rise up and live out the true meaning of its creed: "We hold these truths to be self-evident, that all men are created equal."I have a dream that one day on the red hills of Georgia,the sons of former slaves and the sons of former slave owners will be able to sit down together at the table of brotherhood.I have a dream that one day even the state of Mississippi,a state sweltering with the heat of injustice,sweltering with the heat of oppression,will be transformed into an oasis of freedom and justice.I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character.I have a dream today!I have a dream that one day, down in Alabama, with its vicious racists, with its governor having his lips dripping with the words of "interposition" and "nullification" --one day right there in Alabama little black boys and black girls will be able to join hands with little white boys and white girls as sisters and brothers.I have a dream today!I have a dream that one day every valley shall be exalted, and every hill and mountain shall be made low, the rough places will be made plain, and the crooked places will be made straight; "and the glory of the Lord shall be revealed and all flesh shall see it together."'
 
     },
+
+
     // 滑动
-    changeStatus: function() {
+    changeStatus: function () {
         let that = this;
         that.setData({
             byStentebce: !that.data.byStentebce, // 逐句
@@ -24,11 +26,11 @@ Page({
         // 如果是逐句
         if (that.data.byStentebce) {
             that.setData({
-                sOpacity: 1,  
+                sOpacity: 1,
                 pOpacity: 0,
                 left: 60,
             })
-        } else if (that.data.byParagraph) { 
+        } else if (that.data.byParagraph) {
             that.setData({
                 sOpacity: 0,
                 pOpacity: 1,
