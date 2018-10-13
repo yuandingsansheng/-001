3dfa8757805a6e8ab3f6f9384101a3afd45b298d
yuandingsansheng <975375822@qq.com>
2018-10-13 21:56:08 +0800

    8th

--- pages/dubSingle/dubSingle.js
+++ pages/dubSingle/dubSingle.js
@@ -6,7 +6,6 @@ Page({
     data: {
         byStentebce: false,
         byParagraph: true,
-<<<<<<< HEAD
         sOpacity: 0,
         pOpacity: 1,
         left: 0,
@@ -96,11 +95,6 @@ Page({
             }
         }
     },
-=======
-        videoUrl: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
-        sentence: 'The great pleasure in life is doing what people say you cannot do.',
-        paragraph: 'I have a dream that one day this nation will rise up and live out the true meaning of its creed: "We hold these truths to be self-evident, that all men are created equal."I have a dream that one day on the red hills of Georgia,the sons of former slaves and the sons of former slave owners will be able to sit down together at the table of brotherhood.I have a dream that one day even the state of Mississippi,a state sweltering with the heat of injustice,sweltering with the heat of oppression,will be transformed into an oasis of freedom and justice.I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character.I have a dream today!I have a dream that one day, down in Alabama, with its vicious racists, with its governor having his lips dripping with the words of "interposition" and "nullification" --one day right there in Alabama little black boys and black girls will be able to join hands with little white boys and white girls as sisters and brothers.I have a dream today!I have a dream that one day every valley shall be exalted, and every hill and mountain shall be made low, the rough places will be made plain, and the crooked places will be made straight; "and the glory of the Lord shall be revealed and all flesh shall see it together."'
->>>>>>> 70972dabc76b6c9307ad12dcbd5f2d101502721b
 
     // 配音完成，进入预览界面
     preview:function(){
@@ -113,7 +107,6 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-<<<<<<< HEAD
         let that = this;
         if (that.data.byParagraph) {
             that.setData({
@@ -125,9 +118,6 @@ Page({
             })
         }
 
-=======
-        console.log(options);
->>>>>>> 70972dabc76b6c9307ad12dcbd5f2d101502721b
     },
 
     /**
